7ac2db3f76a9b1dc5f21e94167d0c16ce0e1afbd
Abhishek C <abhishekpalodath05@gmail.com>
2021-06-11 20:20:29 +0530

    agora integration

--- lib/models/agora_room.g.dart
+++ lib/models/agora_room.g.dart
@@ -9,13 +9,11 @@ part of 'agora_room.dart';
 AgoraRoom _$AgoraRoomFromJson(Map<String, dynamic> json) {
   return AgoraRoom(
     Room.fromJson(json['room'] as Map<String, dynamic>),
-    json['channel'] as String,
     json['token'] as String,
   );
 }
 
 Map<String, dynamic> _$AgoraRoomToJson(AgoraRoom instance) => <String, dynamic>{
       'room': instance.room,
-      'channel': instance.channel,
       'token': instance.token,
     };
