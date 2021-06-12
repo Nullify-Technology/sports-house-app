4f9f4a7aefcc07235ccc28ace32232fbe971ff81
Aswin Divakar <aswindivakar100@gmail.com>
2021-06-13 00:33:05 +0530

    Removed Unnecessary Container

--- lib/utils/reusable_components/InRoomBottomBar.dart
+++ lib/utils/reusable_components/InRoomBottomBar.dart
@@ -11,79 +11,74 @@ class InRoomBottomBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Card(
+      margin: EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 5,
+      ),
+      clipBehavior: Clip.hardEdge,
+      color: kInRoomBottomBarBgColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
       child: Container(
-        // height: 75,
-        child: Card(
-          margin: EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 5,
-          ),
-          clipBehavior: Clip.hardEdge,
-          color: kInRoomBottomBarBgColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-          ),
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 5,
+        padding: EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 5,
+        ),
+        child: Row(
+          children: [
+            Container(
+              // color: Colors.white,
+              // alignment: Alignment.center,
+              width: 100,
+              height: 50,
+              child: Stack(
+                alignment: Alignment.centerRight,
+                children: buildProfileStack(members: room.members),
+              ),
             ),
-            child: Row(
-              children: [
-                Container(
-                  // color: Colors.white,
-                  // alignment: Alignment.center,
-                  width: 100,
-                  height: 50,
-                  child: Stack(
-                    alignment: Alignment.centerRight,
-                    children: buildProfileStack(members: room.members),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        room.name,
-                        maxLines: 1,
-                        style: TextStyle(
-                          color: kColorBlack,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        '$kHostedBy ${room.createdBy.name}',
-                        maxLines: 1,
-                        style: TextStyle(
-                          color: kColorBlack,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    context.read<AgoraProvider>().leaveRoom(room.id);
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: new BoxDecoration(
-                      color: kCloseButtonBgColor,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      Icons.close,
-                      color: Colors.red.shade300,
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    room.name,
+                    maxLines: 1,
+                    style: TextStyle(
+                      color: kColorBlack,
+                      fontSize: 16,
                     ),
                   ),
-                )
-              ],
+                  Text(
+                    '$kHostedBy ${room.createdBy.name}',
+                    maxLines: 1,
+                    style: TextStyle(
+                      color: kColorBlack,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+            TextButton(
+              onPressed: () {
+                context.read<AgoraProvider>().leaveRoom(room.id);
+              },
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: new BoxDecoration(
+                  color: kCloseButtonBgColor,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.close,
+                  color: Colors.red.shade300,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
