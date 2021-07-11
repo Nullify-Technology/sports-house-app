bb0c05155d1e5e5462da4cda6ce7f852f33bf011
Abhishek C <abhishekpalodath05@gmail.com>
2021-07-11 13:43:58 +0530

    added video highlight

---
+++ lib/models/api_response.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ApiResponse<T> _$ApiResponseFromJson<T>(
+  Map<String, dynamic> json,
+  T Function(Object? json) fromJsonT,
+) {
+  return ApiResponse<T>(
+    (json['results'] as List<dynamic>).map(fromJsonT).toList(),
+  );
+}
+
+Map<String, dynamic> _$ApiResponseToJson<T>(
+  ApiResponse<T> instance,
+  Object? Function(T value) toJsonT,
+) =>
+    <String, dynamic>{
+      'results': instance.results.map(toJsonT).toList(),
+    };
